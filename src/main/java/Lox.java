2fc2ebbe1872bb6777b3f6c14b96d0830312a78a
mojoboss <animusmojo@gmail.com>
2024-12-07 15:58:04 +0530

    added error reporting in lox.java

--- src/main/java/Lox.java
+++ src/main/java/Lox.java
@@ -13,7 +13,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-
+  static boolean hadError = false;
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -34,9 +34,22 @@ public class Lox {
     }
   }
 
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+  private static void report(int line, String where,
+      String message) {
+    System.err.println(
+        "[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }
+
   private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
+    if(hadError){
+      System.exit(65);
+    }
   }
 
   private static void runPrompt() throws IOException {
@@ -48,6 +61,7 @@ public class Lox {
       if (line == null)
         break;
       run(line);
+      hadError = false;
     }
   }
 
